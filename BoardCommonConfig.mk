3251033a8448e4ab5e9270c060d6920ce4767a0e
ghepeu <ghepeu@gmail.com>
2013-07-01 13:04:05 +0200

    galaxys2: reenable fimg
    Change-Id: I87d90d655fd82f2169f08afeda9d51de607fcab5

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -69,6 +69,10 @@ TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/re
 BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
 USE_OPENGL_RENDERER := true
 
+# FIMG Acceleration
+BOARD_USES_FIMGAPI := true
+BOARD_USES_SKIA_FIMGAPI := true
+
 # Enable WEBGL in WebKit
 ENABLE_WEBGL := true
 
