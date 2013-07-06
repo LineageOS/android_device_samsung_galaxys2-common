2d4c52900e2938db5cf754f96a9929cc266df3e6
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-07-06 13:37:13 +0200

    galaxys2: move libril, use SamsungExynos4RIL class
    Change-Id: Idec0820fa46fabd0d4703fedef3e6b168b96e800

--- common.mk
+++ common.mk
@@ -113,6 +113,7 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
