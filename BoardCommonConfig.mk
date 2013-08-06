6a71b5c1b05261b0289fe989682a5a16e340e21f
XpLoDWilD <xplodwild@cyanogenmod.org>
2013-08-06 21:39:41 +0200

    BoardCommonConfig: Add LEGACY_EGL flag
    Change-Id: Ifa96a6f3846a9ad85dc7eb3a81605b64272f900f

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -70,6 +70,7 @@ TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys2-common/re
 # Graphics
 BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
 USE_OPENGL_RENDERER := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
 
 # FIMG Acceleration
 BOARD_USES_FIMGAPI := true
