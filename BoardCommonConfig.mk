279859988403de649091978b0bff52bce153fd29
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-08-11 16:23:09 +0200

    galaxys2: add selinux policies
    Change-Id: I52a914da94f5c331b4928cb2e79d8fa5a0c0c41c

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -137,6 +137,17 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
+# Selinux
+BOARD_SEPOLICY_DIRS := \
+    device/samsung/galaxys2-common/selinux
+
+BOARD_SEPOLICY_UNION := \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    rild.te
+
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys2-common/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxys2-common/recovery/graphics.c
