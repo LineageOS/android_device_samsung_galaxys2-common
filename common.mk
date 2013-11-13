8974c6de39cde97c3bc8c32b4d2293d13dc5d19a
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-11-13 16:07:17 +0400

    Fix graphical issues and disable SELinux
    Change-Id: Iafeb4349fa504b5a5f7d72afd3274581de5b2ddc

--- common.mk
+++ common.mk
@@ -118,6 +118,8 @@ PRODUCT_COPY_FILES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
+    ro.zygote.disable_gl_preload=true \
+    ro.bq.gpu_to_cpu_unsupported=true \
     debug.hwui.render_dirty_regions=false
 
 # RIL
@@ -126,6 +128,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
+
+# Disable SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.selinux=disabled
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
