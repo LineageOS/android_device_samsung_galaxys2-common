98506f92eb79159a179f8f4e423b595deac0ea3e
l0rdg3x <l0rdg3x@gmail.com>
2013-11-28 00:57:08 +0100

    Fix line 174
    Change-Id: I9d340a3799e8198e2b197ec8e195aa303a85b645

--- common.mk
+++ common.mk
@@ -170,8 +170,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hard
-ware.bluetooth_le.xml
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Feature live wallpaper
 PRODUCT_COPY_FILES += \
