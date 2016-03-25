f94f7a8e0bbbc5eb15db8e72dcb8f67fb8e95578
Caio Schnepper <caioschnepper@gmail.com>
2016-03-24 23:43:38 -0300

    galaxys2-common: Remove low latency audio
    The sound stutter too much with it
    
    Change-Id: If1633e7e7de18f6dd2d9a34165c8c71a563bef45

--- common.mk
+++ common.mk
@@ -166,7 +166,6 @@ PRODUCT_PACKAGES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
