e52704468158654a00e644eb1ef8ef185985de0a
Dominggoes Isakh <drjisakh@gmail.com>
2016-10-21 11:14:03 +0100

    galaxys2-common: Restore specific gps-fields
    * Unknown field in GpsSvInfo in Samsung I9100 libgps.
      May be an indicator for constellation type (GPS, GLONASS, Galileo)?
      Used on GT-I9100, likely also present on GT-N7000, SGH-I717, SGH-I727 but this needs confirmation.
    
    * Add AGPS psc field
      The GPS chipset used in the phone can use the Psc value for AGPS.
      Rename field in AGpsRefLocationCellID struct and add a new define.
      Credit to Qaweck from xda-developers forum for finding the meaning
      of the field.
      Change-Id: Ib917c3b0ebf3c4b0a683fc993ff41521a01b32d4
    
    Change-Id: I1f9f5b0a4eeac01a570462ffdb422400628138d9

--- include/hardware/gps.h
+++ include/hardware/gps.h
@@ -522,6 +522,12 @@ typedef uint8_t                         GnssConstellationType;
  */
 #define GNSS_CONFIGURATION_INTERFACE     "gnss_configuration"
 
+/**
+ * The GPS chipset can use Psc for AGPS.
+ */
+#define AGPS_USE_PSC
+
+
 
 /** Represents a location. */
 typedef struct {
@@ -570,6 +576,14 @@ typedef struct {
     float   elevation;
     /** Azimuth of SV in degrees. */
     float   azimuth;
+    /** Unknown field in Samsung I9100 libgps
+        May be an indicator for constellation type
+        (GPS, GLONASS, Galileo)?
+        Used on GT-I9100, likely also present on GT-N7000,
+        SGH-I717, SGH-I727 but this needs confirmation.
+    */
+    int unknown_samsung_field;
+
 } GpsSvInfo;
 
 typedef struct {
@@ -584,7 +598,7 @@ typedef struct {
      * - GPS:     1-32
      * - SBAS:    120-151, 183-192
      * - GLONASS: 1-24, the orbital slot number (OSN), if known.  Or, if not:
-     *            93-106, the frequency channel number (FCN) (-7 to +6) offset by + 100
+     *            93-106, the fGnssSvInforequency channel number (FCN) (-7 to +6) offset by + 100
      *            i.e. report an FCN of -7 as 93, FCN of 0 as 100, and FCN of +6 as 106.
      * - QZSS:    193-200
      * - Galileo: 1-36
@@ -664,6 +678,9 @@ typedef struct {
      * might rely in the old (wrong) behavior.
      */
     uint16_t lac;
+#ifdef AGPS_USE_PSC
+    uint16_t psc;
+#endif
     /** Cell id in 2G. Utran Cell id in 3G. Cell Global Id EUTRA in LTE. */
     uint32_t cid;
     /** Tracking Area Code in LTE. */
@@ -781,7 +798,6 @@ typedef struct {
     gps_release_wakelock release_wakelock_cb;
     gps_create_thread create_thread_cb;
     gps_request_utc_time request_utc_time_cb;
-
     gnss_set_system_info set_system_info_cb;
     gnss_sv_status_callback gnss_sv_status_cb;
 } GpsCallbacks;
