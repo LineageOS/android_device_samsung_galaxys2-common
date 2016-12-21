6c573dbcdaf0c401c9facb88b89a2a4b8e04eb71
Dominggoes Isakh <drjisakh@gmail.com>
2016-12-21 20:17:35 +0100

    galaxys2-common: Use cache-partition for CM-Recovery
    * Use /cache/recovery/command for bootloader messages because
      there is no misc-partition or similar for this device.
    
    Thanks to Dan Pasanen for his initial code.
    References:
    - https://review.cyanogenmod.org/#/c/171741/
    - https://review.cyanogenmod.org/#/c/172596/
    
    Change-Id: Ie5113ec3a80e1f5bc6ad8a3913657ad3d19dfa0b

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -148,6 +148,7 @@ BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxys2-common/recovery/graphi
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_PROVIDES_BOOTLOADER_MESSAGE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 BOARD_RECOVERY_SWIPE := true
