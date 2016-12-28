ff935f4d3d86ca577a00fead9c43a1bb65e5a573
Dominggoes Isakh <drjisakh@gmail.com>
2016-12-28 15:24:14 +0100

    galaxys2-common: Implement SamsungPowerHAL
    Change-Id: Ic024c159779d74e77841c55a9d7f1bbb505068c9

--- common.mk
+++ common.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.smdk4210
 
+# SamsungPowerHAL
+PRODUCT_PACKAGES += \
+    power.smdk4210
+
 # Audio Packages
 PRODUCT_PACKAGES += \
     AdvancedDisplay \
