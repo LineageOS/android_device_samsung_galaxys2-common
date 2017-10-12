a03d1a5d62e7c8f5635dd513cf0e44cd7b6e03dd
Dominggoes Isakh <drjisakh@gmail.com>
2017-10-12 13:21:03 +0200

    galaxys2-common: Add graphics HIDL HALs
    Change-Id: Ib262f0b2f3137c958d360d9ee3360d5f72c689ce

--- common.mk
+++ common.mk
@@ -88,6 +88,9 @@ PRODUCT_PACKAGES += \
 
 # HAL
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.keymaster@3.0-impl \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor \
