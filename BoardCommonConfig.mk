8aa2623d06785b69176d5e41cccff72ec5e3ef5d
Dominggoes Isakh <drjisakh@gmail.com>
2018-01-28 11:03:49 +0100

    galaxys2-common: Restore displaycolor calibration
    Change-Id: I7ef164837457dbd8b12ad02d2e2dfad073edc5fd

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -87,8 +87,8 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 TARGET_RELEASETOOLS_EXTENSIONS := ./device/samsung/galaxys2-common
 
 # Hardware tunables
-#BOARD_HARDWARE_CLASS := hardware/samsung/cmhw \
-#    device/samsung/galaxys2-common/cmhw
+BOARD_HARDWARE_CLASS := hardware/samsung/lineagehw \
+    device/samsung/galaxys2-common/lineagehw
 
 # Graphics
 BOARD_EGL_CFG := device/samsung/galaxys2-common/configs/egl.cfg
