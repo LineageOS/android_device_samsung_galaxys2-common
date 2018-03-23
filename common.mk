fccfa35d1c3d3897d533480acc34c70e79a82d5c
Dominggoes Isakh <drjisakh@gmail.com>
2018-03-23 21:44:20 +0100

    galaxys2-common: gralloc: Use Exynos4 implementation of interface
    Change-Id: I2451b6428bbc48fa237bc8f8b4baf61cc28b5428

--- common.mk
+++ common.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES += \
 # HAL
 PRODUCT_PACKAGES += \
     AdvancedDisplay \
-    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.keymaster@3.0-impl \
     android.hardware.bluetooth@1.0-impl \
