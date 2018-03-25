3363b69c194d83c18b825f4d184009bad61df26a
Dominggoes Isakh <drjisakh@gmail.com>
2018-03-25 11:40:30 +0200

    galaxys2-common: libc-shim: Don't abort in __pthread_internal_find
    Change-Id: Ie7f62d9c439b24e4708d196d768df2f3bce38e8c

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -64,7 +64,7 @@ endif
 
 # Bionic
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libsec-ril.so|libsamsung_symbols.so
+    /system/lib/libsec-ril.so|libsamsung_symbols.so:system/lib/libc.so|libc-shim.so
 
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
