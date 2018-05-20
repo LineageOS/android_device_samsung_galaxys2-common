a0bcead15ed3dfbb9eb607a0d82d42cd2969fb19
Dominggoes Isakh <drjisakh@gmail.com>
2018-05-20 19:28:58 +0200

    galaxys2-common: Dexopt: Add debug-info
    Change-Id: Ie2b9821b3076c0066935cfdc044ecb4c0fa53633

--- system_prop_debug.mk
+++ system_prop_debug.mk
@@ -2,4 +2,5 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.secure=0 \
     ro.adb.secure=0 \
     ro.debuggable=1 \
-    persist.service.adb.enable=1
+    persist.service.adb.enable=1 \
+    debug.generate-debug-info=1
