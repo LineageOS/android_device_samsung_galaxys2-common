01be3059dfa859794def5b32c8174dcba33118c6
Dominggoes Isakh <drjisakh@gmail.com>
2018-06-04 20:53:47 +0200

    galaxys2-common: Disable ART optimizations, interpret only
    Some apps are crashing after a day because ART optimizations took
    place. The optimized code has issues with catching NullPointerExceptions.
    
    Logcat:
    pid: ....., tid: ....., name: [appname]
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x......
    Cause: null pointer dereference
    
    Change-Id: I10116ea50f4db98632a6906913d4d723ba74b1e2

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -53,13 +53,8 @@ BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_ALLOWS_INVALID_PTHREAD := true
 
-# Don't dex preopt apps to avoid I/O congestion due to paging larger sized
-# pre-compiled .odex files as opposed to background generated interpret-only
-# odex files.
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-
-# Generate debug info
-PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
+# Disable ART optimizations
+WITH_DEXPREOPT := false
 
 # Bionic
 TARGET_LD_SHIM_LIBS := \
