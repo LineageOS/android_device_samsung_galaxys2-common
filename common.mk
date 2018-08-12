58f7ca4eeabf39649d6705a20d289159877127e7
Dominggoes Isakh <drjisakh@gmail.com>
2018-08-12 22:28:29 +0200

    galaxys2-common: Remove AdvancedDisplay for Pie bringup
    Change-Id: I5841976b57289fffac70af64f06b92666f91245e

--- common.mk
+++ common.mk
@@ -82,7 +82,6 @@ PRODUCT_PACKAGES += \
 
 # HAL
 PRODUCT_PACKAGES += \
-    AdvancedDisplay \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl-exynos4 \
     android.hardware.keymaster@3.0-impl \
