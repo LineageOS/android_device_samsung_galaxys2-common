cc52aec00f6f137ca8753e41c8240fd1a542fcfd
Dominggoes Isakh <drjisakh@gmail.com>
2018-08-18 13:38:06 +0200

    galaxys2-common: Disable libstagefright-shim for Pie bringup

--- common.mk
+++ common.mk
@@ -108,8 +108,8 @@ PRODUCT_PACKAGES += \
     camera.smdk4210 \
     Snap
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/mediaserver.rc:system/etc/init/mediaserver.rc
+# PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/configs/mediaserver.rc:system/etc/init/mediaserver.rc
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
