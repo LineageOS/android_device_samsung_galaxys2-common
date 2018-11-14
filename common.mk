4f4dce447ba1f79193e1b760ef7fbd407a537075
Dominggoes Isakh <drjisakh@gmail.com>
2018-11-15 00:10:23 +0100

    galaxys2-common: Tweak memory optimization settings
    Change-Id: I508b384e06e310db22cd0802ed80c79411fb4ddd

--- common.mk
+++ common.mk
@@ -171,11 +171,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Memory Optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.sys.fw.bg_apps_limit=8 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=10 \
     ro.vendor.qti.am.reschedule_service=true \
     ro.vendor.qti.sys.fw.bservice_enable=true \
     ro.vendor.qti.sys.fw.bservice_age=5000 \
-    ro.vendor.qti.sys.fw.bservice_limit=5
+    ro.vendor.qti.sys.fw.bservice_limit=10
 
 # Services
 PRODUCT_PROPERTY_OVERRIDES += \
