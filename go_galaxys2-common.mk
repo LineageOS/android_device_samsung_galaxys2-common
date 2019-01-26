0635ffa6d95bd7912c81919cc5c25a2cf3e16516
Dominggoes Isakh <drjisakh@gmail.com>
2019-01-27 00:00:31 +0100

    galaxys2-common: Use Speed-compilation for System_Server and shared apk's
    Change-Id: I746cf3a0fe6be9909edc1e23c397ab70a012ef53

--- go_galaxys2-common.mk
+++ go_galaxys2-common.mk
@@ -30,8 +30,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
      pm.dexopt.downgrade_after_inactive_days=10
 
 
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+# Speed services and wifi-service for better performance.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
@@ -51,7 +51,7 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 # Some notable apps that will be affected by this are gms and chrome.
 # b/65591595.
 PRODUCT_PROPERTY_OVERRIDES += \
-     pm.dexopt.shared=quicken
+     pm.dexopt.shared=speed
 
 # Default heap sizes. Allow up to 256m for large heaps to make sure a single app
 # doesn't take all of the RAM.
