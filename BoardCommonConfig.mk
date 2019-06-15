8a2382777d650cd88d07da191496cc2e86ca23d4
Dominggoes Isakh <drjisakh@gmail.com>
2019-06-15 22:25:14 +0200

    galaxys2-common: TWRP: Fix BOARD_UMS_LUNFILE
    Change-Id: Ie65db8715535b431392b901f606429bdfdf94a6a

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -151,7 +151,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/samsung/galaxys2-common/selinux/privat
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys2-common/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxys2-common/recovery/graphics.c
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
